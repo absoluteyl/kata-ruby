f4ec6c7c1fde0801372a666c172d29809a1fed9d
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-12 21:21:25 +0800

    feat: should return love fifteen when play2 goals

--- tennis/lib/tennis.rb
+++ tennis/lib/tennis.rb
@@ -1,6 +1,6 @@
 class Tennis
 
-  attr_accessor :player1_score
+  attr_accessor :player1_score, :player2_score
 
   SCORE_STRINGS = {
     1 => 'fifteen',
@@ -10,15 +10,21 @@ class Tennis
 
   def initialize
     @player1_score = 0
+    @player2_score = 0
   end
 
   def score
     return "#{SCORE_STRINGS[@player1_score]} love" if @player1_score != 0
+    return "love #{SCORE_STRINGS[@player2_score]}" if @player2_score != 0
 
     'love all'
   end
 
   def player1_goals
     @player1_score += 1
+  end
+
+  def player2_goals
+    @player2_score += 1
   end
 end
