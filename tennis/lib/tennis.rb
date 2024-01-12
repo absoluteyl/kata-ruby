7505f9fab4c78d05d081d7a0342c70621c8dd78e
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-12 21:07:01 +0800

    refactor: use hash to map score and strings

--- tennis/lib/tennis.rb
+++ tennis/lib/tennis.rb
@@ -2,13 +2,17 @@ class Tennis
 
   attr_accessor :player1_score
 
+  SCORE_STRINGS = {
+    15 => 'fifteen',
+    30 => 'thirty'
+  }
+
   def initialize
     @player1_score = 0
   end
 
   def score
-    return 'fifteen love' if @player1_score == 15
-    return 'thirty love' if @player1_score == 30
+    return "#{SCORE_STRINGS[@player1_score]} love" if @player1_score != 0
 
     'love all'
   end
