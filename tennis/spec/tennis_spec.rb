62a6220f34617a71601cb6b076d980df2c01b8d7
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-12 20:58:26 +0800

    refactor: init Tennis before each example

--- tennis/spec/tennis_spec.rb
+++ tennis/spec/tennis_spec.rb
@@ -1,17 +1,22 @@
 require 'tennis'
 
 RSpec.describe Tennis, '#score' do
+
+  before do
+    @tennis = Tennis.new
+  end
+
   context 'when initialize' do
     it 'should be love all' do
-      expect(Tennis.new.score).to eq('love all')
+      expect(@tennis.score).to eq('love all')
     end
   end
 
   context 'when player 1 goals' do
     it 'should be fifteen love' do
-      tennis = Tennis.new
-      tennis.player1_goals
-      expect(tennis.score).to eq('fifteen love')
+      @tennis
+      @tennis.player1_goals
+      expect(@tennis.score).to eq('fifteen love')
     end
   end
 end
