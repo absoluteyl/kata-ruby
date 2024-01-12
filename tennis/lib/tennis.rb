4a75b92848f1573087e22671097d60e1f96e360c
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-12 21:00:32 +0800

    feat: return fifteen love after player1 goals

--- tennis/lib/tennis.rb
+++ tennis/lib/tennis.rb
@@ -1,5 +1,18 @@
 class Tennis
+
+  attr_accessor :player1_score
+
+  def initialize
+    @player1_score = 0
+  end
+
   def score
+    return 'fifteen love' if @player1_score == 15
+
     'love all'
+  end
+
+  def player1_goals
+    @player1_score += 15
   end
 end
