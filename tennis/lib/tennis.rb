92063f9ced7721d3a839d892ceb976c29561e672
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-13 11:03:51 +0800

    feat: should return deuce when score is 3:3

--- tennis/lib/tennis.rb
+++ tennis/lib/tennis.rb
@@ -15,7 +15,13 @@ class Tennis
   end
 
   def score
-    return "#{SCORE_STRINGS[@player1_score]} all"  if @player1_score == @player2_score
+    if @player1_score == @player2_score
+      if @player1_score < 3
+        return "#{SCORE_STRINGS[@player1_score]} all"
+      else
+        return "deuce"
+      end
+    end
     "#{SCORE_STRINGS[@player1_score]} #{SCORE_STRINGS[@player2_score]}" if @player1_score != 0 || @player2_score != 0
   end
 
